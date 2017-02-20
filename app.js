73f586d50646d15ddd07fb4dd95f4358be1c398a
Carlos Cadena <cs.cadena@gmail.com>
2017-02-20 15:16:13 -0800

    constructor built with each image as an object

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-use strict;
+'use strict';
+
+function Product(productName, productPath) {
+  this.productName = productName;
+  this.productPath = productPath;
+}
+
+var bag = new Product('R2D2 Luggage', 'img/bag.jpg');
+var banana = new Product('Banana Slicer', 'img/banana.jpg');
+var bathroom = new Product('TP/IPad Holder', 'img/bathroom.jpg');
+var boots = new Product('Toeless Boots', 'img/boots.jpg');
+var breakfast = new Product('Toaster/Fryer/Coffee Maker','img/breakfast.jpg');
+var bubblegum = new Product('Meatbull Bubblegum', 'img/bubblegum.jpg');
+var chair = new Product('Red Chair', 'img/chair.jpg');
+var cthulhu = new Product('Cthulhu', 'img/cthulhu.jpg');
+var dogDuck = new Product('Dog Duckbill', 'img/dog-duck.jpg');
+var dragon = new Product('Dragon Meat', 'img/dragon.jpg');
+var pen = new Product('Utensil Pen Caps', 'img/pen.jpg');
+var petSweep = new Product('Paw Sweeper', 'img/pet-sweep.jpg');
+var scissors = new Product('Pizza Scissors', 'img/scissors.jpg');
+var shark = new Product('Shark Sleeping Bag', 'img/shark.jpg');
+var sweep = new Product('Baby Onesie Sweeper', 'img/sweep.png');
+var tauntaun = new Product('Tauntaun Sleeping Bag', 'img/tauntaun.jpg');
+var unicorn = new Product('Unicorn Meat', 'img/unicorn.jpg');
+var usb = new Product('Tentacle USB', 'img/usb.gif');
+var waterCan = new Product('Water Can', 'img/water-can.jpg');
+var wineGlass = new Product('Wine Glass', 'img/wine-glass.jpg');
+
+
+
+
+
+var imageOne = document.getElementById('image-one');
+var imageTwo = document.getElementById('image-two');
+var imageThree = documents.getElementById('image-three');
